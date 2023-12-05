d7fef001d071654d94cd722ef0ae6f530a5d5ea1
fuRan <NgeKaworu@gmail.com>
2023-12-05 14:10:54 +0800

    chore: main route

---
+++ lib/src/screen/my.dart
@@ -0,0 +1,22 @@
+/*
+ * @Author: fuRan NgeKaworu@gmail.com
+ * @Date: 2023-12-01 13:50:16
+ * @LastEditors: fuRan NgeKaworu@gmail.com
+ * @LastEditTime: 2023-12-05 14:03:29
+ * @FilePath: /flashcard/lib/src/screen/my.dart
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+import 'package:flutter/widgets.dart';
+
+class My extends StatelessWidget {
+  const My({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text("My page"),
+    );
+  }
+}
