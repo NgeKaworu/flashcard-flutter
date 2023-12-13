f4c92c8541bbf335f7aea2e49b75cf4161a84822
fuRan <NgeKaworu@gmail.com>
2023-12-13 18:08:09 +0800

    chore: 拦截器

--- lib/src/client/dio.dart
+++ lib/src/client/dio.dart
@@ -7,5 +7,21 @@ void initDio() {
   final dio = Dio();
   dio.interceptors.add(GetIt.instance<TalkerDioLogger>());
 
+  dio.interceptors.add(InterceptorsWrapper(
+    onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
+      // 如果你想完成请求并返回一些自定义数据，你可以使用 `handler.resolve(response)`。
+      // 如果你想终止请求并触发一个错误，你可以使用 `handler.reject(error)`。
+      return handler.next(options);
+    },
+    onResponse: (Response response, ResponseInterceptorHandler handler) {
+      // 如果你想终止请求并触发一个错误，你可以使用 `handler.reject(error)`。
+      return handler.next(response);
+    },
+    onError: (DioException error, ErrorInterceptorHandler handler) {
+      // 如果你想完成请求并返回一些自定义数据，你可以使用 `handler.resolve(response)`。
+      return handler.next(error);
+    },
+  ));
+
   GetIt.instance<Talker>().info('Repositories initialization completed');
 }
